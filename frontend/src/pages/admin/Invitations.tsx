78d69984765a1bae1fe85c7deda2a95d7394659f
Suven-p <pandeysuven@gmail.com>
2023-08-12 19:21:04 +0545

    User full page table on invitations

--- frontend/src/pages/admin/Invitations.tsx
+++ frontend/src/pages/admin/Invitations.tsx
@@ -117,7 +117,7 @@ const PendingRegistrationTab = ({
         },
     ];
     return (
-        <Box sx={{ height: 400, width: '100%' }}>
+        <Box sx={{ height: '85%', width: '100%' }}>
             <DataGrid
                 rows={rows}
                 columns={columns}
